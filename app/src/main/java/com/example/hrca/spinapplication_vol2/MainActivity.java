dc67a723ef5fbae14134d92a77bc0b9414454434
Hrvoje <nemcanin@yahoo.com>
2016-12-30 20:56:54 +0000

    kodak poppin

--- app/src/main/java/com/example/hrca/spinapplication_vol2/MainActivity.java
+++ app/src/main/java/com/example/hrca/spinapplication_vol2/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.hrca.spinapplication_vol2;
 
+import android.app.Fragment;
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,9 +18,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.hrca.spinapplication_vol2.fragments.ChartFragment;
+import com.example.hrca.spinapplication_vol2.fragments.GetMealFragment;
+import com.example.hrca.spinapplication_vol2.fragments.LoginFragment;
+import com.example.hrca.spinapplication_vol2.fragments.ProfileFragment;
+import com.example.hrca.spinapplication_vol2.helpers.Constants;
+import com.example.hrca.spinapplication_vol2.model.Food;
+
+import mehdi.sakout.fancybuttons.FancyButton;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, ChartFragment.OnFragmentInteractionListener {
+
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +41,9 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        pref= PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -41,6 +53,33 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FancyButton fancyButton = new FancyButton(this);
+        fancyButton=(FancyButton)findViewById(R.id.btn_search_for_food);
+        fancyButton.setIconResource(R.drawable.ic_search_white_24dp);
+        fancyButton.setFontIconSize(40);
+
+        FancyButton fancyLoginButton=new FancyButton(this);
+        fancyLoginButton=(FancyButton)findViewById(R.id.login_or_register_button);
+        fancyButton.setFontIconSize(40);
+
+        fancyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this, FoodActivity.class);
+                startActivity(i);
+            }
+        });
+
+        fancyLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this, LoginOrRegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //initFragment();
     }
 
     @Override
@@ -94,16 +133,43 @@ public class MainActivity extends AppCompatActivity
         }
 
         else if (id == R.id.nav_login_register) {
+
+
+
+
+
             Intent loginIntent=new Intent(this, LoginOrRegisterActivity.class);
             startActivity(loginIntent);
-        } else if (id == R.id.nav_meal_history) {
+
+        }
+
+
+        else if (id == R.id.nav_meal_history) {
             Intent loginIntent=new Intent(this, DietCalendarActivity.class);
             startActivity(loginIntent);
 
         } else if (id == R.id.nav_nutritions) {
 
-            Intent loginIntent=new Intent(this, NutritionDataActivity.class);
-            startActivity(loginIntent);
+
+
+
+            if(pref.getBoolean(Constants.IS_LOGGED_IN,false)){
+
+
+                Intent loginIntent=new Intent(this, NutritionDataActivity.class);
+                startActivity(loginIntent);
+
+            }else {
+
+
+                Intent loginIntent=new Intent(this, LoginOrRegisterActivity.class);
+                startActivity(loginIntent);
+
+            }
+
+
+
+
 
         }
         //else if (id == R.id.nav_manage) {
@@ -118,5 +184,24 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void initFragment(){
+        Fragment fragment;
+        if(pref.getBoolean(Constants.IS_LOGGED_IN,false)){
+            ChartFragment chartFragment=ChartFragment.newInstance("0", "");
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainFrame,chartFragment);
+            fragmentTransaction.commit();
+        }else {
+            Toast.makeText(this, "Go To Login Page", Toast.LENGTH_LONG);
+        }
+
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
